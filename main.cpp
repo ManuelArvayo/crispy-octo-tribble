9184e2ed86fa7021543d35438dd111c8a4ff047b
ManuelArvayo <61918631+ManuelArvayo@users.noreply.github.com>
2020-03-07 15:01:04 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cmath>
 using namespace std;
 
 
@@ -10,17 +11,19 @@ ifstream arEncriptado;
 ofstream arSalida;
 char a,b,c,d,e,f,g,h,i,j,k,l,m,n;
 string x, y, z;
-int num;
+int num=0, num1=0, resultado=0;
 
 arEncriptado.open("entrada1.txt");
-
+arSalida.open("Salida1.txt");
 arEncriptado>>a>>b>>c>>d>>e>>f>>g>>h>>i>>j>>k>>l>>m>>n;
 
 
 while(arEncriptado>>x>>y>>z){
 
-for(int i= 0; i<=x.length(); i++){
-if(x[i]==66)
+
+for(int i= 0; i<x.length(); i++){
+  
+if(x[i]==66) //Condicional para convertir de B a 0
   x[i]=x[i]-18;
 if(x[i]==121)
   x[i]=x[i]-72;
@@ -40,17 +43,11 @@ if(x[i]==80)
   x[i]=x[i]-24;
 if(x[i]==70)
   x[i]=x[i]-13;
-// if(x[i]==67)
-//   x[i]=x[i]-24;
-// if(x[i]==99)
-//   x[i]=x[i]-54;
-// if(x[i]==35)
-//   x[i]=x[i]+7;
-// if(x[i]==64)
-//   x[i]=x[i]-17;  
+num+=(x[i]-48)*(pow(10,(x.length()-i-1)));
 }
 
-for(int i2= 0; i2<=z.length(); i2++){
+for(int i2= 0; i2<z.length(); i2++)
+{
 if(z[i2]==66)
   z[i2]=z[i2]-18;
 if(z[i2]==121)
@@ -71,25 +68,34 @@ if(z[i2]==80)
   z[i2]=z[i2]-24;
 if(z[i2]==70)
   z[i2]=z[i2]-13;
+
+num1+=(z[i2]-48)*(pow(10,z.length()-i2-1));
+
+
 }
 for(int i1= 0; i1<=y.length(); i1++){
 if(y[i1]==67){
   y[i1]-=24;
+  resultado=num+num1;
 }
 if(y[i1]==99){
    y[i1]-=54;
+   resultado=num-num1;
 }
 if(y[i1]==35){
   y[i1]+=7;
+  resultado=num*num1;
 }
 if(y[i1]==64){
   y[i1]-=17;
+  resultado=num/num1;
 }
 }
 
 
-cout<<"("<<x<<")"<<" "<<y<<" "<<"("<<z<<")"<<endl;
-
+arSalida<<"("<<x<<")"<<" "<<y<<" "<<"("<<z<<")"<<" = "<<resultado<<endl;
+num=0;
+num1=0;
 }
 // cout<<a<<b<<c<<d<<e<<f<<g<<h<<i<<j<<k<<l<<m<<n;
 
