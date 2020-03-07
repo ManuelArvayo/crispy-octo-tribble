821a2655c979203b7f96ccfee542e67bd74295f2
ManuelArvayo <61918631+ManuelArvayo@users.noreply.github.com>
2020-03-07 15:02:42 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -9,13 +9,18 @@ int main() {
   
 ifstream arEncriptado;
 ofstream arSalida;
-char a,b,c,d,e,f,g,h,i,j,k,l,m,n;
-string x, y, z;
+char a,b,c,d,e,f,g,h,i9,j,k,l,m,n;
+string x, y, z, nombreEntrada, nombreSalida;
 int num=0, num1=0, resultado=0;
 
-arEncriptado.open("entrada1.txt");
-arSalida.open("Salida1.txt");
-arEncriptado>>a>>b>>c>>d>>e>>f>>g>>h>>i>>j>>k>>l>>m>>n;
+cout<<"Ingrese el nombre del archivo a decodificar"<<endl;
+cin>>nombreEntrada;
+cout<<"Ingrese el nombre del archivo final"<<endl;
+cin>>nombreSalida;
+arEncriptado.open(nombreEntrada.c_str());
+arSalida.open(nombreSalida.c_str());
+
+arEncriptado>>a>>b>>c>>d>>e>>f>>g>>h>>i9>>j>>k>>l>>m>>n;
 
 
 while(arEncriptado>>x>>y>>z){
@@ -23,71 +28,70 @@ while(arEncriptado>>x>>y>>z){
 
 for(int i= 0; i<x.length(); i++){
   
-if(x[i]==66) //Condicional para convertir de B a 0
-  x[i]=x[i]-18;
-if(x[i]==121)
-  x[i]=x[i]-72;
-if(x[i]==47)
-  x[i]=x[i]+3;
-if(x[i]==56)
-  x[i]=x[i]-5;
-if(x[i]==84)
-  x[i]=x[i]-32;
-if(x[i]==38)
-  x[i]=x[i]+15;
-if(x[i]==81)
-  x[i]=x[i]-27;
-if(x[i]==69)
-  x[i]=x[i]-14;
-if(x[i]==80)
-  x[i]=x[i]-24;
-if(x[i]==70)
-  x[i]=x[i]-13;
+if(x[i]==a) //Condicional para convertir de B a 0
+  x[i]='0';
+if(x[i]==b)
+  x[i]='1';
+if(x[i]==c)
+  x[i]='2';
+if(x[i]==d)
+  x[i]='3';
+if(x[i]==e)
+  x[i]='4';
+if(x[i]==f)
+  x[i]='5';
+if(x[i]==g)
+  x[i]='6';
+if(x[i]==h)
+  x[i]='7';
+if(x[i]==i9)
+  x[i]=56;
+if(x[i]==j)
+  x[i]='9';
 num+=(x[i]-48)*(pow(10,(x.length()-i-1)));
 }
 
 for(int i2= 0; i2<z.length(); i2++)
 {
-if(z[i2]==66)
-  z[i2]=z[i2]-18;
-if(z[i2]==121)
-  z[i2]=z[i2]-72;
-if(z[i2]==47)
-  z[i2]=z[i2]+3;
-if(z[i2]==56)
-  z[i2]=z[i2]-5;
-if(z[i2]==84)
-  z[i2]=z[i2]-32;
-if(z[i2]==38)
-  z[i2]=z[i2]+15;
-if(z[i2]==81)
-  z[i2]=z[i2]-27;
-if(z[i2]==69)
-  z[i2]=z[i2]-14;
-if(z[i2]==80)
-  z[i2]=z[i2]-24;
-if(z[i2]==70)
-  z[i2]=z[i2]-13;
-
+if(z[i2]==a) //Condi2cional para convertir de B a 0
+  z[i2]='0';
+if(z[i2]==b)
+  z[i2]='1';
+if(z[i2]==c)
+  z[i2]='2';
+if(z[i2]==d)
+  z[i2]='3';
+if(z[i2]==e)
+  z[i2]='4';
+if(z[i2]==f)
+  z[i2]='5';
+if(z[i2]==g)
+  z[i2]='6';
+if(z[i2]==h)
+  z[i2]='7';
+if(z[i2]==i9)
+  z[i2]=56;
+if(z[i2]==j)
+  z[i2]='9';
 num1+=(z[i2]-48)*(pow(10,z.length()-i2-1));
 
 
 }
 for(int i1= 0; i1<=y.length(); i1++){
-if(y[i1]==67){
-  y[i1]-=24;
+if(y[i1]==k){
+  y[i1]='+';
   resultado=num+num1;
 }
-if(y[i1]==99){
-   y[i1]-=54;
+if(y[i1]==l){
+   y[i1]='-';
    resultado=num-num1;
 }
-if(y[i1]==35){
-  y[i1]+=7;
+if(y[i1]==m){
+  y[i1]='*';
   resultado=num*num1;
 }
-if(y[i1]==64){
-  y[i1]-=17;
+if(y[i1]==n){
+  y[i1]='/';
   resultado=num/num1;
 }
 }
